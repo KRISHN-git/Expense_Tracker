880e81cd7bedf2530466c7f43e55f041e1b4c028
KRISHN <krishnkr07@gmail.com>
2026-02-18 16:08:11 +0530

    Frontend Setup: React, Tailwind, API Service

---
+++ frontend/src/services/api.js
@@ -0,0 +1,25 @@
+
+import axios from 'axios';
+
+const API_URL = 'http://localhost:5000/expenses';
+
+const api = axios.create({
+    baseURL: API_URL,
+});
+
+export const getExpenses = async (params) => {
+    const response = await api.get('/', { params });
+    return response.data;
+};
+
+export const createExpense = async (expenseData, idempotencyKey) => {
+    const config = {
+        headers: {
+            'Idempotency-Key': idempotencyKey,
+        },
+    };
+    const response = await api.post('/', expenseData, config);
+    return response.data;
+};
+
+export default api;
